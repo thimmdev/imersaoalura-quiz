b22726b2b5c4e3a948f0007fcd5ff677bcf11676
thimmdmev <thiagomm.dev@gmail.com>
2021-01-25 19:56:35 -0300

    Logo - Mobile version

--- source/components/Logo/index.js
+++ source/components/Logo/index.js
@@ -6,12 +6,23 @@ width: 500px;
 align-items: center;
 justify-content: center;
 
-
 img {
     width: 100%;
     margin: 50px 0 0 250px
   }
+
+  @media (max-width: 600px) {
+    img {
+        margin: 100px 50px 0;
+        width: 80%;
+    }
+}
+
 `;
+
+
+
+
 
 export default function Logo(props){
     return <LogoWrapper {...props}>
